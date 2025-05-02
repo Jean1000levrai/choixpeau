0d3c9dfd0fb6304c333c381a672ddc95160bce6f
Jean1000 <157409715+Jean1000levrai@users.noreply.github.com>
2025-05-02 19:23:52 +0200

    Add files via upload

--- choixpeau_magique/function.py
+++ choixpeau_magique/function.py
@@ -2,6 +2,8 @@ import csv
 
 # -----------Choixpeau ai--------------
 def open_csv(path):
+    """retourne une nouvelle instance de 
+    la base de donnée des élèves"""
     list_csv = []
     with open(path, 'r') as file:
         lec = csv.DictReader(file)
@@ -31,30 +33,38 @@ def maison_majoritaire(list_eleves):
     return rep
 
 def distance_eleves(e1, e2):
+    """retourne la distance de manhattan de 2 élèves"""
     return (abs(int(e2["Courage"])-int(e1["Courage"]))
     + abs(int(e2["Loyaute"])-int(e1["Loyaute"]))
     + abs(int(e2["Sagesse"])-int(e1["Sagesse"]))
     + abs(int(e2["Malice"])-int(e1["Malice"])))
 
 def plus_proche_voisin(eleve, db, k = 7):
+    """retourne une liste contenant les k plus proches
+      élèves de la base de donnée d'un autres élèves donné
+      selon l'algorithme de la distance de manhattan"""
     eleve_proche = None
     l = []
     for _ in range(k):
         dist = 999999
         for i in range(len(db)):
+        # on boucle sur tous les élèves de la base
             dist2 = distance_eleves(eleve, db[i])
             if dist2 < dist and db[i] not in l:
+            # on compare les distance à tous les élèves
+            # on choisit le plus proche qui n'est pas déjà dans la liste
                 dist = dist2
                 eleve_proche = db[i]
         l.append(eleve_proche)
     return l
 
 def attribution_maison(eleve, list_eleves):
+    """retourne une maison pour un élève donné en paramètre"""
     voisin = plus_proche_voisin(eleve, list_eleves)
     maison = maison_majoritaire(voisin)
     return maison
 
-# -------------else---------------
+# -------------test---------------
 if __name__ == "__main__":
     db_path = "choixpeauMagique.csv"
     list_csv = open_csv(db_path)
--- choixpeau_magique/main.py
+++ choixpeau_magique/main.py
@@ -4,29 +4,35 @@ import gui
 
 
 if __name__ == "__main__":
-    # main
+    # -------------------main-------------------
     db_path = "choixpeauMagique.csv"
     list_csv = open_csv(db_path)
 
-    # gui
-    root = tk.Tk()
-    height = 400
-    width = 800
-    canvas = tk.Canvas(width=width, height=height, master=root)
     def ask_btn_func():
+        """fonction qui demande à l'apprenti sorcier ses infos"""
         gui.ask(canvas)
         ask_btn.pack_forget()
         speak_btn.pack()
     def speak_btn_func():
+        """fonction qui utilise l'animation du choixpeau :
+        il donne la maison étant attribuée ou renvoie une erreur si 
+        les infos ne sont pas données correctement"""
         gui.choixpeau_speaking_anim(1, canvas,12)
         try:
-            gui.saying(f"Hum... Humm...!\nHummmmmmmmmm!! \n{gui.eleve['Nom']} tu es .......\n{attribution_maison(gui.eleve, list_csv)}!!", canvas,12)
+            gui.saying(f"Hum... Humm...!\nHummmmmmmmmm!! \n{gui.eleve['Nom']}, tu es .......\n{attribution_maison(gui.eleve, list_csv)}!!", canvas,12)
         except : 
             gui.saying("Sois sérieux voyons!", canvas, 12)
+
+    # -------------------gui-------------------
+    root = tk.Tk()
+    height = 400
+    width = 800
+    canvas = tk.Canvas(width=width, height=height, master=root)
+
     speak_btn = tk.Button(text="Speak", master=root, command = speak_btn_func)
     ask_btn = tk.Button(text="ask", master=root, command = ask_btn_func)
 
-    # launching
+    # -------------------launching-------------------
     gui.create_choixpeau("img/choixpeau_img_", 200, 300, canvas, root)
     canvas.pack()
     ask_btn.pack()
